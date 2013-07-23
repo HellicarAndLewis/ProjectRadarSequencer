2b4feab565df40b592dee8f3a3123a1c064f5fb5
Kyle McDonald <kyle@kylemcdonald.net>
2013-07-23 19:56:33 -0400

    basic ui framework

--- RadarSequencer/src/main.cpp
+++ RadarSequencer/src/main.cpp
@@ -6,10 +6,34 @@
 #include "ofxMidi.h"
 #include "ofxUI.h"
 
+ofPoint sickOffset;
+float sickRotation = 0;
+bool useRecording = false;
+
 class ofApp : public ofBaseApp {
-public:
-	void setup() {
+public:	
+	ofxSick *sickA, *sickB;
+	ofxSickPlayer sickPlayerA, sickPlayerB;
+	ofxSickGrabber sickGrabberA, sickGrabberB;
+	ofxUICanvas* gui;
 	
+	void setup() {
+		sickA = &sickPlayerA;
+		sickB = &sickPlayerB;
+		setupGui();
+	}
+	void setupGui() {
+		gui = new ofxUICanvas();
+		gui->setTheme(OFX_UI_THEME_MINBLACK);
+		gui->addLabel("Radar Sequencer");
+		gui->addSpacer();
+		gui->addFPS();
+		gui->addSpacer();
+		gui->addLabelToggle("Use recording", &useRecording);
+		gui->addSpacer();
+		gui->addMinimalSlider("Sick rotation", 0, 360, &sickRotation);
+		gui->add2DPad("Sick offset", ofVec2f(-250, 250), ofVec2f(-250, 250), &sickOffset, 100, 100);
+		gui->autoSizeToFitWidgets();
 	}
 	void update() {
 		
