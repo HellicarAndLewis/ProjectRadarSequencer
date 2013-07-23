ac55bee1f117316f8725670bb2d3acefbe126671
Kyle McDonald <kyle@kylemcdonald.net>
2013-07-23 19:44:05 -0400

    added project file contain all necessary addons

---
+++ RadarSequencer/src/main.cpp
@@ -0,0 +1,25 @@
+#include "ofMain.h"
+
+#include "ofxCv.h"
+#include "ofxDmx.h"
+#include "ofxSick.h"
+#include "ofxMidi.h"
+#include "ofxUI.h"
+
+class ofApp : public ofBaseApp {
+public:
+	void setup() {
+	
+	}
+	void update() {
+		
+	}
+	void draw() {
+		
+	}
+};
+
+int main( ){
+	ofSetupOpenGL(1280, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
