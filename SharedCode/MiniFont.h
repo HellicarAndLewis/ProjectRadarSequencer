3d6e34ed3d435f7ab8616ecdeac545ace7a13881
Kyle McDonald <kyle@kylemcdonald.net>
2013-08-10 01:41:28 +0900

    voronoi ranges for pulse edges

---
+++ SharedCode/MiniFont.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ofMain.h"
+
+namespace MiniFont {
+	void updateProjectionState();
+	ofVec3f ofWorldToScreen(ofVec3f world);
+	void setup();
+	void draw(string str, int x, int y);
+	void drawHighlight(string str, const ofVec2f& position, ofColor bg = ofColor(0), ofColor fg = ofColor(255));
+	void drawHighlight(string str, int x, int y, ofColor bg = ofColor(0), ofColor fg = ofColor(255));
+	void drawScreen(string str, ofVec3f position);
+}
--- Simulation/src/main.cpp
+++ Simulation/src/main.cpp
@@ -1,16 +1,100 @@
 #include "ofMain.h"
+#include "MiniFont.h"
 
 float pxToMm = 1. / 10.;
+float carWidth = 2600, tireWidth = 550, tireHeight = 180, tireSpacing = 845;
+float directionSize = 10000, triangleSide = 50000;
+float outerEllipseWidth = 4600, outerEllipseHeight = 3480;
+float innerEllipseWidth = 3840, innerEllipseHeight = 2720;
+float peopleRadius = 10;
+float offsetAmount = 600;
+
+class DraggableCircle {
+protected:
+	bool selected;
+	ofVec2f position;
+	static int idCount;
+	int id;
+public:
+	DraggableCircle()
+	:selected(false) {
+	}
+	virtual void setup() {
+		id = idCount++;
+		ofAddListener(ofEvents().mousePressed, this, &DraggableCircle::mousePressed);
+		ofAddListener(ofEvents().mouseMoved, this, &DraggableCircle::mouseMoved);
+		ofAddListener(ofEvents().mouseDragged, this, &DraggableCircle::mouseDragged);
+		ofAddListener(ofEvents().draw, this, &DraggableCircle::draw);
+	}
+	bool getSelected() const {
+		return selected;
+	}
+	ofVec2f& getPosition() {
+		return position;
+	}
+	void mousePressed(ofMouseEventArgs& e) {
+		mouseMoved(e);
+	}
+	void mouseMoved(ofMouseEventArgs& e) {
+		selected = position.distance(e) < peopleRadius;
+	}
+	void mouseDragged(ofMouseEventArgs& e) {
+		if(selected) {
+			position.set(e);
+		}
+	}
+	void draw(ofEventArgs& e) {
+		ofPushStyle();
+		ofFill();
+		ofSetColor(255);
+		ofCircle(position, peopleRadius);
+		if(selected) {
+			ofSetColor(ofColor::red);
+			ofNoFill();
+			ofCircle(position, peopleRadius * 2);				
+		}
+		MiniFont::drawHighlight(ofToString(id), position);
+		ofPopStyle();
+	}
+};
+
+int DraggableCircle::idCount = 0;
+
+class Person : public DraggableCircle {
+public:
+	virtual void setup(ofVec2f position) {
+		DraggableCircle::setup();
+		this->position = position;
+	}
+	ofVec2f getWorldPosition() const {
+		return position - ofGetWindowSize() / 2;
+	}
+	float getAngle() const {
+		return getWorldPosition().angle(ofVec2f(1, 0));
+	}
+};
+
+bool operator < (const Person& a, const Person& b) {
+	return a.getAngle() < b.getAngle();
+}
 
 class ofApp : public ofBaseApp {
 public:
+	vector<Person> people;
 	
 	void setup() {
 		ofSetCircleResolution(64);
 		ofSetLineWidth(2);
+		
+		MiniFont::setup();
+		
+		people.resize(5);
+		for(int i = 0; i < people.size(); i++) {
+			people[i].setup(ofVec2f(ofRandomWidth(), ofRandomHeight()));
+		}
 	}
 	void update() {
-		
+		ofSort(people);
 	}
 	void drawTriangle(ofVec2f center, ofVec2f p1, ofVec2f p2, float side) {
 		p1 = center + (p1 - center).normalize() * side;
@@ -19,31 +103,45 @@ public:
 	}
 	void draw() {
 		ofBackground(0);
-		ofSetColor(255, 128);
-		ofNoFill();
+		
+		ofPushMatrix();
 		ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
 		ofScale(pxToMm, pxToMm);
 		ofRotate(45);
+		drawCar();
+		ofPopMatrix();
 		
-		float offsetAmount = mouseX; // variable
-		float carWidth = 2600, tireWidth = 550, tireHeight = 180, tireSpacing = 845;
-		float directionSize = 10000, triangleSide = 50000;
-		float outerEllipseWidth = 4600, outerEllipseHeight = 3480;
-		float innerEllipseWidth = 3840, innerEllipseHeight = 2720;
+		ofPushMatrix();
+		ofPushStyle();
+		ofSetColor(ofColor::cyan);
+		ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+		int n = people.size();
+		for(int i = 0; i < n; i++) {
+			float a = people[i].getAngle();
+			float b = people[(i + 1) % n].getAngle();
+			while(b < a) {
+				b += 360;
+			}
+			float avg = (a + b) / 2;
+			avg = fmodf(avg, 360);
+			ofLine(ofVec2f(120, 0).rotate(-avg), ofVec2f(300, 0).rotate(-avg)); 
+		}
+		ofPopStyle();
+		ofPopMatrix();
 		
+		MiniFont::drawHighlight(ofToString(mouseX) + " " + ofToString(mouseY), 10, 10);
+	}
+	void drawCar() {
 		ofVec2f flt(-carWidth / 2 - tireWidth / 2, +tireSpacing - tireHeight), brt(carWidth / 2 - tireWidth / 2, -tireSpacing);
 		ofVec2f topCenter = (flt + brt) / 2, topOffsetDirection = (brt - flt).normalize();
 		ofVec2f topSensorCenter = topCenter + topOffsetDirection * offsetAmount;
 		ofVec2f topSensorDirection = topOffsetDirection.rotate(-90);
-		
 		ofVec2f flb(-carWidth / 2 + tireWidth / 2, +tireSpacing), brb(carWidth / 2 + tireWidth / 2, -tireSpacing + tireHeight);
 		ofVec2f bottomCenter = (flb + brb) / 2, bottomOffsetDirection = (flb - brb).normalize();
 		ofVec2f bottomSensorCenter = bottomCenter + bottomOffsetDirection * offsetAmount;
 		ofVec2f bottomSensorDirection = bottomOffsetDirection.rotate(-90);
-		
 		ofVec2f bl(+carWidth / 2 - tireWidth / 2, +tireSpacing);
 		ofVec2f br(+carWidth / 2 + tireWidth / 2, +tireSpacing - tireHeight);
-		
 		ofVec2f tl(-carWidth / 2 - tireWidth / 2, -tireSpacing + tireHeight);
 		ofVec2f tr(-carWidth / 2 + tireWidth / 2, -tireSpacing);
 		
@@ -69,10 +167,11 @@ public:
 		ofCircle(bottomSensorCenter, 50);
 		ofPopStyle();
 		
+		ofPushStyle();
+		ofSetColor(255, 128);
+		ofNoFill();
 		ofEllipse(0, 0, outerEllipseWidth, outerEllipseHeight); // outer
 		ofEllipse(0, 0, innerEllipseWidth, innerEllipseHeight); // inner
-		
-		ofPushStyle();
 		ofSetRectMode(OF_RECTMODE_CENTER);
 		ofRectRounded(0, 0, 4100, 1690, 300);
 		ofRect(-carWidth / 2, +tireSpacing - tireHeight / 2, tireWidth, tireHeight);
@@ -80,8 +179,6 @@ public:
 		ofRect(-carWidth / 2, -tireSpacing + tireHeight / 2, tireWidth, tireHeight);
 		ofRect(+carWidth / 2, -tireSpacing + tireHeight / 2, tireWidth, tireHeight);
 		ofPopStyle();
-		
-		ofDrawBitmapString(ofToString(mouseX) + " " + ofToString(mouseY), 10, 20);
 	}
 };
 
